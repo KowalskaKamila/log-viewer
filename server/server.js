6fe4af107c0bf8fa4201e3daf8e59dd042ff5175
Artem Vovk <vovk.artem@gmail.com>
2020-10-20 15:33:14 -1000

    Submit initial server version

---
+++ server/server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var cors = require('cors');
+const { once } = require('events');
+const path = require('path'); 
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/logs', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    let all_logs = [];
+    fs.readdirSync('.').forEach(file => {
+        if (path.extname(file) == ".log") {
+            all_logs.push({filename: file});
+        }
+    });
+    res.send(JSON.stringify(all_logs));
+})
+
+let file_cache = {}
+
+app.get('/logs/:file_name', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(req.params.file_name)
+    });
+  
+    let lines = []
+    // for (const line of lineReader) {
+    //     // Each line in input.txt will be successively available here as `line`.
+    //     console.log(`Line from file: ${line}`);
+    //     lines.push(line)
+    // }
+    lineReader.on('line', function (line) {
+        lines.push(line)
+    });
+    lineReader.on('close', function() {
+        res.send(JSON.stringify(lines))
+    });
+    
+})
+
+var server = app.listen(8083, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
