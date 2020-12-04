8abd29abbec686ece4835df784e4f64c1f7f870c
Artem Vovk <vovk.artem@gmail.com>
2020-12-03 16:22:45 -0800

    Add an option to serve react code through same node server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const path = require('path');
 
 app.use(express.json())
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.get('/logs', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
